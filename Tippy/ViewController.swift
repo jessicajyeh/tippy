83ffe4839d8c7fa40a29b7e60a5663c31b12b8a7
Jessica Yeh <jessicayeh@fb.com>
2017-06-20 15:15:39 -0700

    created basic UI layout

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(_ sender: Any) {
+
+    }
 }
 
